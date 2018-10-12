d10cb9efb2a0c5a498254dd7dcc28f1f540b27a7
Mark Lopez <m@silvenga.com>
2018-10-11 21:39:19 -0500

    Created stopwatch abstraction.

---
+++ src/Spectaris/Core/Timepiece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Spectaris.Core
+{
+    public interface ITimepiece
+    {
+        TimeSpan Elapsed { get; }
+        long ElapsedMilliseconds { get; }
+        long ElapsedTicks { get; }
+        bool IsRunning { get; }
+
+        void Reset();
+        void Restart();
+        void Start();
+        void Stop();
+    }
+
+    public class Timepiece : ITimepiece
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        public long ElapsedMilliseconds => _stopwatch.ElapsedMilliseconds;
+        public long ElapsedTicks => _stopwatch.ElapsedTicks;
+        public TimeSpan Elapsed => _stopwatch.Elapsed;
+        public bool IsRunning => _stopwatch.IsRunning;
+
+        public void Reset() => _stopwatch.Reset();
+        public void Restart() => _stopwatch.Restart();
+        public void Start() => _stopwatch.Start();
+        public void Stop() => _stopwatch.Stop();
+    }
+}
