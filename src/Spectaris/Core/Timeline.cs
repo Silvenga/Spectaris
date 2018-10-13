0b67c05a1c23c7f2430fd60ed04b050c0f9d10b0
Mark Lopez <m@silvenga.com>
2018-10-12 23:48:46 -0500

    Added timeline abstraction, added coverage.

---
+++ src/Spectaris/Core/Timeline.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Spectaris.Core
+{
+    public interface ITimeline
+    {
+        TimeSpan GetCurrentTick();
+        TimeSpan GetTimeSinceTick(TimeSpan tick);
+    }
+
+    public class Timeline
+    {
+        private readonly ITimepiece _timepiece;
+
+        public Timeline(ITimepiece timepiece)
+        {
+            _timepiece = timepiece;
+            _timepiece.Start();
+        }
+
+        public TimeSpan GetCurrentTick()
+        {
+            return _timepiece.Elapsed;
+        }
+
+        public TimeSpan GetTimeSinceTick(TimeSpan tick)
+        {
+            var currentTick = GetCurrentTick();
+            return currentTick - tick;
+        }
+    }
+}
---
+++ tests/Spectaris.Tests/Core/TimelineFacts.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using AutoFixture;
+
+using FluentAssertions;
+
+using NSubstitute;
+
+using Spectaris.Core;
+
+using Xunit;
+
+namespace Spectaris.Tests.Core
+{
+    public class TimelineFacts
+    {
+        private static readonly Fixture Autofixture = new Fixture();
+
+        [Fact]
+        public void When_timeline_is_constructed_then_start_timepiece()
+        {
+            var timepieceMock = Substitute.For<ITimepiece>();
+
+            // Act
+            // ReSharper disable once ObjectCreationAsStatement
+            new Timeline(timepieceMock);
+
+            // Assert
+            timepieceMock.Received().Start();
+        }
+
+        [Fact]
+        public void When_tick_is_requested_then_return_current_timer_time()
+        {
+            var timepieceMock = Substitute.For<ITimepiece>();
+            var timeline = new Timeline(timepieceMock);
+
+            var expected = Autofixture.Create<TimeSpan>();
+            timepieceMock.Elapsed.Returns(expected);
+
+            // Act
+            var result = timeline.GetCurrentTick();
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void When_time_since_tick_is_requested_then_return_span()
+        {
+            var timepieceMock = Substitute.For<ITimepiece>();
+            var timeline = new Timeline(timepieceMock);
+
+            var lastTick = Autofixture.Create<TimeSpan>();
+            var expected = Autofixture.Create<TimeSpan>();
+            var currentTick = lastTick + expected;
+            timepieceMock.Elapsed.Returns(currentTick);
+
+            // Act
+            var result = timeline.GetTimeSinceTick(lastTick);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
