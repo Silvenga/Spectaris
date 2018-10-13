5cea787a05277bbfe31d855b45de2c28442a2672
Mark Lopez <m@silvenga.com>
2018-10-12 22:00:53 -0500

    Created basic HttpModule prototype.

---
+++ src/Spectaris/HttpModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+namespace Spectaris
+{
+    public class HttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += BeginRequest;
+        }
+
+        private void BeginRequest(object sender, EventArgs e)
+        {
+            var application = (HttpApplication) sender;
+            var context = application.Context;
+            context.Response.AddOnSendingHeaders(httpContext => httpContext.Response.Headers.Add("X-Handled", "true"));
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
