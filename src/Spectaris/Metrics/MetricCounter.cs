2b66f9299df1d2607c58e99ed36e5d830fde6b80
Mark Lopez <m@silvenga.com>
2018-10-14 11:23:43 -0500

    Added metric counter, added coverage.

---
+++ src/Spectaris/Metrics/MetricCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+
+namespace Spectaris.Metrics
+{
+    public class MetricCounter
+    {
+        // ReSharper disable once ArgumentsStyleLiteral
+        private SpinLock _spinLock = new SpinLock(enableThreadOwnerTracking: true);
+
+        public long Total { get; private set; }
+        public long Count { get; private set; }
+        public long Minimum { get; private set; } = long.MaxValue;
+        public long Maximum { get; private set; } = long.MinValue;
+        public decimal Average { get; private set; }
+
+        public void AddMeasurement(long value)
+        {
+            var lockTaken = false;
+            try
+            {
+                _spinLock.Enter(ref lockTaken);
+
+                Total += value;
+                Count++;
+                Minimum = value < Minimum ? value : Minimum;
+                Maximum = value > Maximum ? value : Maximum;
+                Average = Total / (decimal)Count;
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    _spinLock.Exit(false);
+                }
+            }
+        }
+    }
+}
---
+++ tests/Spectaris.Tests/Metrics/MetricCounterFacts.cs
@@ -0,0 +1,83 @@
+﻿using AutoFixture;
+
+using FluentAssertions;
+
+using Spectaris.Metrics;
+
+using Xunit;
+
+namespace Spectaris.Tests.Metrics
+{
+    public class MetricCounterFacts
+    {
+        private static readonly Fixture Autofixture = new Fixture();
+        private readonly MetricCounter _metricCounter = new MetricCounter();
+
+        [Fact]
+        public void When_a_metric_is_added_then_count_should_increment()
+        {
+            // Act
+            _metricCounter.AddMeasurement(Autofixture.Create<long>());
+
+            // Assert
+            _metricCounter.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void When_a_metric_is_added_then_total_should_increment()
+        {
+            var measurementFake = Autofixture.Create<long>();
+
+            // Act
+            _metricCounter.AddMeasurement(measurementFake);
+
+            // Assert
+            _metricCounter.Total.Should().Be(measurementFake);
+        }
+
+        [Fact]
+        public void When_multiple_metrics_are_added_then_average_should_be_an_average_of_all_measurements()
+        {
+            var measurementFake1 = Autofixture.Create<long>();
+            var measurementFake2 = Autofixture.Create<long>();
+
+            // Act
+            _metricCounter.AddMeasurement(measurementFake1);
+            _metricCounter.AddMeasurement(measurementFake2);
+
+            // Assert
+            var expected = (measurementFake1 + measurementFake2) / 2m;
+            _metricCounter.Average.Should().Be(expected);
+        }
+
+        [Fact]
+        public void When_a_metric_is_added_and_the_metric_is_smaller_then_current_min_then_min_should_be_the_measurement()
+        {
+            const int measurementFake1 = 10;
+            const int measurementFake2 = 5;
+
+            _metricCounter.AddMeasurement(measurementFake1);
+
+            // Act
+            _metricCounter.AddMeasurement(measurementFake2);
+
+            // Assert
+            _metricCounter.Minimum.Should().Be(measurementFake2);
+        }
+
+        [Fact]
+        public void When_a_metric_is_added_and_the_metric_is_larger_then_current_max_then_max_should_be_the_measurement()
+        {
+            const int measurementFake1 = 5;
+            const int measurementFake2 = 10;
+
+            _metricCounter.AddMeasurement(measurementFake1);
+
+            // Act
+            _metricCounter.AddMeasurement(measurementFake2);
+
+            // Assert
+            _metricCounter.Maximum.Should().Be(measurementFake2);
+        }
+    }
+}
