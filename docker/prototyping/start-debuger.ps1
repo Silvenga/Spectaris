e833800b12b85835f2ff386b94e8ed7c0c8ca88b
Mark Lopez <m@silvenga.com>
2018-10-27 21:16:03 -0500

    Fixed debugger path.

--- docker/prototyping/start-debuger.ps1
+++ docker/prototyping/start-debuger.ps1
@@ -1,1 +1,1 @@
-docker exec -it example_iis_1 "C:\Program Files\Microsoft Visual Studio 15.0\Common7\IDE\Remote Debugger\x64\msvsmon.exe" /nostatus /silent /noauth /anyuser /nosecuritywarn
+docker exec -it prototyping_iis_1 "C:\Program Files\Microsoft Visual Studio 15.0\Common7\IDE\Remote Debugger\x64\msvsmon.exe" /nostatus /silent /noauth /anyuser /nosecuritywarn
